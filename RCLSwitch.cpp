1fe02387a57fc224b3f5b59c2f6a96c4d257ba79
NicoHood <nico-2006@web.de>
2014-05-28 20:20:56 +0200

    update

--- RCLSwitch.cpp
+++ RCLSwitch.cpp
@@ -1,44 +1,48 @@
 /*
 This is a minimal sketch without using the library at all but only works for
- the 10 pole dip switch sockets. It saves a lot of memory and thus might be
- very useful to use with ATTinys :)
- 
- http://code.google.com/p/rc-switch/
- */
+the 10 pole dip switch sockets. It saves a lot of memory and thus might be
+very useful to use with ATTinys :)
+
+Copyright (c) 2014 NicoHood.  All right reserved.
+
+Original library:
+http://code.google.com/p/rc-switch/
+*/
 
 #include "RCLSwitch.h"
 
-RCLSwitch::RCLSwitch(uint8_t RCLpin){
-  _RCLpin=RCLpin;
-  pinMode(_RCLpin, OUTPUT);
-}
 
-void RCLSwitch::changeInput(uint16_t &code) {
-  if ((code&B1)==0) code--; //if ending is 10(on) -> 01(off), substract 1
-  else code++;
-  sendInput(code);
-}
+// inline
+//void RCLSwitch::changeInput(uint16_t &code) {
+//// if ending is 10(on) -> 01(off), substract 1
+//	if ((code&B1)==0) code--; 
+//	else code++;
+//	sendInput(code);
+//}
+//void RCLSwitch::sendInput(uint16_t code) {
+//	for (int nRepeat=0; nRepeat<6; nRepeat++) {
+//		for (int i=11; i>=0; i--) { //check every input bit
+//			transmit(1,3); //always send this before Tristate (part of Tri-State actually)
+//			if (code>>i &0x01==1) {
+//				transmit(1,3); //send Tri-State'0'
+//			} 
+//			else {
+//				transmit(3,1); //send Tri-State'F'
+//			}
+//		}
+//		transmit(1,31); //sendSync
+//	}
+//}
+//
 
-void RCLSwitch::sendInput(uint16_t code) {
-  for (int nRepeat=0; nRepeat<6; nRepeat++) {
-    for (int i=11; i>=0; i--) { //check every input bit
-      transmit(1,3); //always send this before Tristate (part of Tri-State actually)
-      if (code>>i &B1==1) {
-        transmit(1,3); //send Tri-State'0'
-      } 
-      else {
-        transmit(3,1); //send Tri-State'F'
-      }
-    }
-    transmit(1,31); //sendSync
-  }
-}
+
+
 
 void RCLSwitch::transmit(uint8_t nHighPulses, uint8_t nLowPulses) {
-  digitalWrite(_RCLpin, HIGH);
-  delayMicroseconds( 350 * nHighPulses);
-  digitalWrite(_RCLpin, LOW);
-  delayMicroseconds( 350 * nLowPulses);
+	digitalWrite(_RCLpin, HIGH);
+	delayMicroseconds( 350 * nHighPulses);
+	digitalWrite(_RCLpin, LOW);
+	delayMicroseconds( 350 * nLowPulses);
 }
 
 
--- RCLSwitch.h
+++ RCLSwitch.h
@@ -1,10 +1,13 @@
 /*
 This is a minimal sketch without using the library at all but only works for
- the 10 pole dip switch sockets. It saves a lot of memory and thus might be
- very useful to use with ATTinys :)
- 
- http://code.google.com/p/rc-switch/
- */
+the 10 pole dip switch sockets. It saves a lot of memory and thus might be
+very useful to use with ATTinys :)
+
+Copyright (c) 2014 NicoHood.  All right reserved.
+
+Original library:
+http://code.google.com/p/rc-switch/
+*/
 
 #ifndef RCLSwitch_h
 #define RCLSwitch_h
@@ -13,13 +16,57 @@ This is a minimal sketch without using the library at all but only works for
 
 class RCLSwitch {
 public:
-  RCLSwitch(uint8_t RCLpin);
-  void changeInput(uint16_t &code);
-  void sendInput(uint16_t code);
+	inline RCLSwitch(uint8_t RCLpin){
+		_RCLpin=RCLpin;
+		pinMode(_RCLpin, OUTPUT);
+	};
+
+	inline void changeInput(uint16_t &code){
+		//if ending is 10(on) -> 01(off), substract 1
+		if ((code&B1)==0) code--; 
+		else code++;
+		sendInput(code);
+	};
+
+	inline void sendInput(uint16_t code){
+		for (int nRepeat=0; nRepeat<6; nRepeat++) {
+			for (int i=11; i>=0; i--) { //check every input bit
+				transmit(1,3); //always send this before Tristate (part of Tri-State actually)
+				if ((code>>i) &0x01==1) {
+					transmit(1,3); //send Tri-State'0'
+				} 
+				else {
+					transmit(3,1); //send Tri-State'F'
+				}
+			}
+			transmit(1,31); //sendSync
+		}
+	};
+
+	//just a test
+	inline void write(uint16_t letter, uint8_t &state){
+		state^=1;
+		uint16_t code =  (letter<<2) | (1 << state) ;
+
+
+				for (int nRepeat=0; nRepeat<6; nRepeat++) {
+			for (int i=11; i>=0; i--) { //check every input bit
+				transmit(1,3); //always send this before Tristate (part of Tri-State actually)
+				if ((code>>i) &0x01==1) {
+					transmit(1,3); //send Tri-State'0'
+				} 
+				else {
+					transmit(3,1); //send Tri-State'F'
+				}
+			}
+			transmit(1,31); //sendSync
+		}
+	};
 
 private:
-  uint8_t _RCLpin;
-  void transmit(uint8_t nHighPulses, uint8_t nLowPulses);
+	uint8_t _RCLpin;
+
+	void transmit(uint8_t nHighPulses, uint8_t nLowPulses);
 };
 
 #endif
